f230c54014e3c7c4bbb92c5b14c56136d2e05b93
ZPLOVEXYH <1031626336@qq.com>
2017-05-11 15:38:16 +0800

    springboot测试

---
+++ src/main/java/com/sping/boot/bean/TestService.java
@@ -0,0 +1,14 @@
+package com.sping.boot.bean;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by Administrator on 2017/5/11.
+ */
+@Service
+public class TestService {
+
+    public String test() {
+        return "test";
+    }
+}
